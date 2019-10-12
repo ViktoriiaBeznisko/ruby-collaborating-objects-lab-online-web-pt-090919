af29d787a57dfcc71e2b9d2724da41fb2b2ea9d0
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-12 02:29:39 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,18 @@
+class MP3Importer
+  
+  attr_reader :path
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def files ###NEED CORRECTION ??
+    #loads all the mp3 files in the path directory
+    #normalizes the filename to just the mp3 filename with no path
+ end
+ 
+  def import
+    files.each {|file| Song.new_by_filename(file)}
+  end
+ 
+end
---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+  
+  attr_accessor :artist, :name
+   
+    @@all = []
+     
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+    
+    def self.all
+      @@all
+    end
+    
+    #def self.new_by_filename(file) ###NEED CORRECTION ??
+   #   song = self.new
+    #  song.name = file.split(" - ")[1]
+ # end
+   
+  def self.new_by_filename(file)
+    song_name = file.split(" - ")[1]
+    artist = file.split(" - ")[0]
+    song = self.new(song_name)
+  end
+   
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+end
