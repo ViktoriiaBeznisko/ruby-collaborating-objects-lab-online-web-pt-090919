b827215ae572a7db185ac3893e652334a59f74a6
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-12 02:33:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,15 +15,13 @@ class Song
     
   def self.new_by_filename(file)
     song.name = file.split(" - ")[1]
-    artist.name = file.split(" - ")[0]
-    song_name = artist.self
-  end
-   
-  def self.new_by_filename(file)
-    song_name = file.split(" - ")[1]
     artist = file.split(" - ")[0]
     song = self.new(song_name)
   end
+   
+ # def self.new_by_filename(file)
+ #   song_name = file.split(" - ")[1]
+ # end
    
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
