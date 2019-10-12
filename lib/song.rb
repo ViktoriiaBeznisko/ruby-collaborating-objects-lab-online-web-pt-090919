ec06e5052786b942d9f1285e004a9513a168f513
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-12 02:36:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,6 +21,10 @@ class Song
     song
   end
    
+   def self.new_by_filename(file)
+    song_name = file.split(" - ")[1]
+    artist = file.split(" - ")[0]
+    song = self.new(song_name)
    
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
